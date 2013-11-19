be55b34e089faefcfe195016a0a37227831a0523
Denis Stepulenok <super.denis@gmail.com>
2013-11-19 20:05:40 +0400

    Добавили модульный тест для вычисления Факториала

---
+++ CSharp/WindowsDemo/Snake/F.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Snake
+{
+    /// <summary>
+    /// Этот класс не относится к игре, он для демонстрации работы с 
+    /// модульными тестами (unit-tests)
+    /// </summary>
+    static public class F
+    {               
+        public static long Fact(int N)
+        {
+            if(N == 4)
+                return 1*2*3*4;
+            if(N == 3)
+                return 6;
+            if(N == 2) 
+                return 2;
+            return 1;
+        }
+    }
+}
--- CSharp/WindowsDemo/Snake/Tests/TestExample.cs
+++ CSharp/WindowsDemo/Snake/Tests/TestExample.cs
@@ -25,5 +25,24 @@ namespace Snake.Tests
         {
             Assert.AreEqual(4, 2*2);
         }
+        
+        /// <summary>
+        /// Test Driven Development - Разработка через тестирование
+        ///   1. Пишем тест 
+        ///   2. Пишем код
+        ///   3. Рефакторинг 
+        /// </summary>
+        [Test]
+        public void TestFactorial(){
+            // 1-ый тест: 1! = 1
+            Assert.AreEqual(1, F.Fact(1));
+            // 2-ый тест: 2! = 2
+            Assert.AreEqual(2, F.Fact(2));
+            // 3-ый тест: 3! = 1*2*3
+            Assert.AreEqual(1*2*3, F.Fact(3));
+            // 4-ый тест: 4! = 1*2*3*4
+            Assert.AreEqual(1*2*3*4, F.Fact(4));
+        }
+        
     }
 }
