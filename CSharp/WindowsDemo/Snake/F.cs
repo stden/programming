af8c4ac3e75730a2fdf263fc1f764f0089fe4dfa
Denis Stepulenok <super.denis@gmail.com>
2013-11-20 00:24:30 +0400

    Рефакторинг

--- CSharp/WindowsDemo/Snake/F.cs
+++ CSharp/WindowsDemo/Snake/F.cs
@@ -3,101 +3,111 @@
 namespace Snake
 {
     /// <summary>
-    /// Этот класс не относится к игре, он для демонстрации работы с 
+    /// Этот класс не относится к игре "Змейка", он для демонстрации работы с
     /// модульными тестами (unit-tests)
     /// </summary>
-    static public class F
-    {               
-        public static long Fact(int N)
+    public static class F
+    {
+        // Бесконечно малое число для сравнений
+        private const double Eps = 1e-20;
+
+        // Кеш для чисел Фибоначчи
+        private static readonly long[] cashe = new long[1000];
+
+        public static long Fact(int n)
         {
-            if(N >= 2) 
-                return N*Fact(N-1);
+            if (n >= 2)
+                return n*Fact(n - 1);
             return 1;
         }
-        
-        static long[] cashe = new long[1000];
-        
-        public static long Fib(int N)
+
+        public static long Fibonacci(int n)
         {
             // Если значение уже вычислялось => сразу возвращаем его            
-            if(cashe[N]!=0)
-                return cashe[N];
-            
+            if (cashe[n] != 0)
+                return cashe[n];
+
             // Если не вычислялось => 1. Вычисляем
             long result;
-            if(N >= 3) 
-                result = Fib(N-1) + Fib(N-2);
-            else 
-                result = 1;
-            
+            if (n >= 3)
+                result = Fibonacci(n - 1) + Fibonacci(n - 2);
+            else
+                result = n == 0 ? 0 : 1;
+
             // 2. Сохраняем в кеш
-            cashe[N] = result; 
-            
+            cashe[n] = result;
+
             // 3. Возвращаем
             return result;
         }
-        
-        public static long FibSlow(int N)
-        {
-            if(N >= 3) 
-                return FibSlow(N-1) + FibSlow(N-2);
-            return 1;            
-        }
-        
-        const double eps = 1e-20;
-            
+
         /// <summary>
-        /// Проверка корней квадратного уравнения Ax^2+Bx+C=0;
+        ///     Медленное вычисление числа Фибоначчи
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static long FibSlow(int n)
+        {
+            if (n >= 3)
+                return FibSlow(n - 1) + FibSlow(n - 2);
+            return n == 0 ? 0 : 1;
+        }
+
+        /// <summary>
+        ///     Проверка корней квадратного уравнения Ax^2+Bx+C=0;
         /// </summary>
         /// <param name="a"></param>
         /// <param name="b"></param>
         /// <param name="c"></param>
-        /// <param name="xx">Массив корней</param>
-        public static void checkRoots(double a, double b, double c, double[] xx){
-            foreach(double x in xx){
-                double res = a*x*x + b*x + c;
-                if(Math.Abs(res) > eps)
-                    throw new Exception("Корень "+x+" не подходит "+a+"x^2 + "+b+"x + "+c+" = 0");
+        /// <param name="roots">Массив корней</param>
+        public static void CheckRoots(double a, double b, double c, double[] roots)
+        {
+            foreach (var x in roots)
+            {
+                var res = a*x*x + b*x + c;
+                if (Math.Abs(res) > Eps)
+                    throw new Exception("Корень " + x + " не подходит " + a + "x^2 + " + b + "x + " + c + " = 0");
             }
             // Все корни подходят => ничего не делаем
         }
-        
+
         /// <summary>
-        /// Решение квадратного уравнения 
+        ///     Решение квадратного уравнения ax^2+bx+c=0
         /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <param name="c"></param>
+        /// <param name="a">Коэффициент a</param>
+        /// <param name="b">Коэффициент b</param>
+        /// <param name="c">Коэффициент c</param>
         /// <returns></returns>
-        public static double[] roots(double a, double b, double c){
-            if(Math.Abs(a) < eps){
-                if(Math.Abs(b) < eps){
-                    if(Math.Abs(c) < eps)
-                        throw new Exception("x-любое");
-                    return new double[0]{};                    
-                } else {                                        
-                    return new double[1]{ -c / a };
-                }                              
+        public static double[] Roots(double a, double b, double c)
+        {
+            if (Math.Abs(a) < Eps)
+            {
+                if (Math.Abs(b) > Eps) return new[] {-c/a};
+                if (Math.Abs(c) < Eps)
+                    throw new Exception("x-любое");
+                return new double[] {};
             }
-            
-            double D = Math.Pow(b,2) - 4*a*c;
-            if(Math.Abs(D) < eps)
-                return new double[1]{ -b/(2*a) };
-            if(D > 0)
-                return new double[2]{  
-                  (-b+Math.Sqrt(D))/(2*a),
-                  (-b-Math.Sqrt(D))/(2*a) 
+
+            // Вычисляем Дискриминант 
+            double D = Math.Pow(b, 2) - 4*a*c;
+            if (Math.Abs(D) < Eps)
+                return new[] {-b/(2*a)};
+            if (D > 0)
+                return new[]
+                {
+                    (-b + Math.Sqrt(D))/(2*a),
+                    (-b - Math.Sqrt(D))/(2*a)
                 };
-            
-            return new double[0]{};
+
+            return new double[] {};
         }
-        
-        public static void checkEq(double a, double b, double c)
+
+        public static void CheckEquation(double a, double b, double c)
         {
             // Вычисляем корни
-            double[] xx = roots(a,b,c);
+            double[] xx = Roots(a, b, c);
             // Проверяем корни уравнения
-            checkRoots(a,b,c,xx);
+            CheckRoots(a, b, c, xx);
         }
     }
 }
--- CSharp/WindowsDemo/Snake/Tests/TestExample.cs
+++ CSharp/WindowsDemo/Snake/Tests/TestExample.cs
@@ -21,20 +21,15 @@ namespace Snake.Tests
             // TODO: Add tear down code.
         }
 
-        [Test]
-        public void TestMethod()
-        {
-            Assert.AreEqual(4, 2*2);
-        }
-        
         /// <summary>
-        /// Test Driven Development - Разработка через тестирование
-        ///   1. Пишем тест 
-        ///   2. Пишем код
-        ///   3. Рефакторинг 
+        ///     Test Driven Development - Разработка через тестирование
+        ///     1. Пишем тест
+        ///     2. Пишем код
+        ///     3. Рефакторинг
         /// </summary>
         [Test]
-        public void TestFactorial(){
+        public void TestFactorial()
+        {
             // 1-ый тест: 1! = 1
             Assert.AreEqual(1, F.Fact(1));
             // 2-ой тест: 2! = 2
@@ -50,46 +45,64 @@ namespace Snake.Tests
             // 7-ой тест: 11! 
             Assert.AreEqual(1*2*3*4*5*6*7*8*9*10*11, F.Fact(11));
             // 8-ой тест: 15! 
-            Assert.AreEqual(1L*2*3*4*5*6*7*8*9*10*11*12*13*14*15, 
-                            F.Fact(15));
+            Assert.AreEqual(1L*2*3*4*5*6*7*8*9*10*11*12*13*14*15,
+                F.Fact(15));
         }
 
         /// <summary>
-        /// Числа Фибоначчи
+        ///     Числа Фибоначчи - элементы числовой последовательности:
+        ///     0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946
+        ///     в которой каждое последующее число равно сумме двух предыдущих чисел.
         /// </summary>
         [Test]
-        public void TestFibonachi(){
-            Assert.AreEqual(1, F.Fib(1));
-            Assert.AreEqual(1, F.Fib(2));
-            Assert.AreEqual(1+1, F.Fib(3));
-            Assert.AreEqual(1+1+1, F.Fib(4));
-            Assert.AreEqual(433494437, F.Fib(43));
-            Assert.AreEqual(2880067194370816120, F.Fib(90));
-            
-            for(int i=1;;i++)
-                System.Console.WriteLine(""+i+"-ое число Фибоначчи = "+F.Fib(i));
-        }        
-        
-        /// <summary>
-        /// Квадратное уравнение
-        /// </summary>
-        [Test]
-        public void TestSquareEq(){                               
-            F.checkEq(1,0,0);
-            F.checkEq(1,-2,1); // x^2 - 2*x + 1
-            F.checkEq(1,2,1); 
-            F.checkEq(1,0,4); // Нет решений
-            
-            F.checkEq(0,0,4); // Нет решений
-            Assert.AreEqual(0, F.roots(0,0,4).Length );
-            
-            F.checkEq(0,1,4); // Один корень 
-            Assert.AreEqual(1, F.roots(0,1,4).Length );
+        public void TestFibonacci()
+        {
+            Assert.AreEqual(0, F.Fibonacci(0));
+            Assert.AreEqual(1, F.Fibonacci(1));
+            Assert.AreEqual(1, F.Fibonacci(2));
+            Assert.AreEqual(1 + 1, F.Fibonacci(3));
+            Assert.AreEqual(1 + 1 + 1, F.Fibonacci(4));
+            Assert.AreEqual(433494437, F.Fibonacci(43));
+            Assert.AreEqual(2880067194370816120, F.Fibonacci(90));
 
-            try{            
-                F.checkEq(0,0,0); // Бесконечное число корней
+            for (int i = 1;; i++)
+                Console.WriteLine("" + i + "-ое число Фибоначчи = " + F.Fibonacci(i));
+        }
+
+        /// <summary>
+        /// Простейший тест
+        /// </summary>
+        [Test]
+        public void TestMethod()
+        {
+            Assert.AreEqual(4, 2*2);
+        }
+
+        /// <summary>
+        ///     Квадратное уравнение
+        /// </summary>
+        [Test]
+        public void TestSquareEq()
+        {
+            F.CheckEquation(1, 0, 0);
+            F.CheckEquation(1, -2, 1); // x^2 - 2*x + 1
+            F.CheckEquation(1, 2, 1);
+            F.CheckEquation(1, 0, 4); // Нет решений
+
+            F.CheckEquation(0, 0, 4); // Нет решений
+            Assert.AreEqual(0, F.Roots(0, 0, 4).Length);
+
+            F.CheckEquation(0, 1, 4); // Один корень 
+            Assert.AreEqual(1, F.Roots(0, 1, 4).Length);
+
+            // Проверка генерации исключения
+            try
+            {
+                F.CheckEquation(0, 0, 0); // Бесконечное число корней
                 Assert.Fail("Ожидалось исключение");
-            } catch(Exception ex){
+            }
+            catch (Exception ex)
+            {
                 Assert.AreEqual("x-любое", ex.Message);
             }
         }
