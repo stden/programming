69f6f2155c29533567d0d4fa5983f4092480d001
Denis Stepulenok <super.denis@gmail.com>
2013-11-19 21:04:38 +0400

    Примеры тестов

--- CSharp/WindowsDemo/Snake/F.cs
+++ CSharp/WindowsDemo/Snake/F.cs
@@ -10,13 +10,94 @@ namespace Snake
     {               
         public static long Fact(int N)
         {
-            if(N == 4)
-                return 1*2*3*4;
-            if(N == 3)
-                return 6;
-            if(N == 2) 
-                return 2;
+            if(N >= 2) 
+                return N*Fact(N-1);
             return 1;
+        }
+        
+        static long[] cashe = new long[1000];
+        
+        public static long Fib(int N)
+        {
+            // Если значение уже вычислялось => сразу возвращаем его            
+            if(cashe[N]!=0)
+                return cashe[N];
+            
+            // Если не вычислялось => 1. Вычисляем
+            long result;
+            if(N >= 3) 
+                result = Fib(N-1) + Fib(N-2);
+            else 
+                result = 1;
+            
+            // 2. Сохраняем в кеш
+            cashe[N] = result; 
+            
+            // 3. Возвращаем
+            return result;
+        }
+        
+        public static long FibSlow(int N)
+        {
+            if(N >= 3) 
+                return FibSlow(N-1) + FibSlow(N-2);
+            return 1;            
+        }
+        
+        const double eps = 1e-20;
+            
+        /// <summary>
+        /// Проверка корней квадратного уравнения Ax^2+Bx+C=0;
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="xx">Массив корней</param>
+        public static void checkRoots(double a, double b, double c, double[] xx){
+            foreach(double x in xx){
+                double res = a*x*x + b*x + c;
+                if(Math.Abs(res) > eps)
+                    throw new Exception("Корень "+x+" не подходит "+a+"x^2 + "+b+"x + "+c+" = 0");
+            }
+            // Все корни подходят => ничего не делаем
+        }
+        
+        /// <summary>
+        /// Решение квадратного уравнения 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static double[] roots(double a, double b, double c){
+            if(Math.Abs(a) < eps){
+                if(Math.Abs(b) < eps){
+                    if(Math.Abs(c) < eps)
+                        throw new Exception("x-любое");
+                    return new double[0]{};                    
+                } else {                                        
+                    return new double[1]{ -c / a };
+                }                              
+            }
+            
+            double D = Math.Pow(b,2) - 4*a*c;
+            if(Math.Abs(D) < eps)
+                return new double[1]{ -b/(2*a) };
+            if(D > 0)
+                return new double[2]{  
+                  (-b+Math.Sqrt(D))/(2*a),
+                  (-b-Math.Sqrt(D))/(2*a) 
+                };
+            
+            return new double[0]{};
+        }
+        
+        public static void checkEq(double a, double b, double c)
+        {
+            // Вычисляем корни
+            double[] xx = roots(a,b,c);
+            // Проверяем корни уравнения
+            checkRoots(a,b,c,xx);
         }
     }
 }
--- CSharp/WindowsDemo/Snake/Tests/TestExample.cs
+++ CSharp/WindowsDemo/Snake/Tests/TestExample.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace Snake.Tests
 {
@@ -36,13 +37,61 @@ namespace Snake.Tests
         public void TestFactorial(){
             // 1-ый тест: 1! = 1
             Assert.AreEqual(1, F.Fact(1));
-            // 2-ый тест: 2! = 2
+            // 2-ой тест: 2! = 2
             Assert.AreEqual(2, F.Fact(2));
-            // 3-ый тест: 3! = 1*2*3
+            // 3-ий тест: 3! = 1*2*3
             Assert.AreEqual(1*2*3, F.Fact(3));
             // 4-ый тест: 4! = 1*2*3*4
             Assert.AreEqual(1*2*3*4, F.Fact(4));
+            // 5-ый тест: 5! = 1*2*3*4*5
+            Assert.AreEqual(1*2*3*4*5, F.Fact(5));
+            // 6-ой тест: 6! = 1*2*3*4*5*6
+            Assert.AreEqual(1*2*3*4*5*6, F.Fact(6));
+            // 7-ой тест: 11! 
+            Assert.AreEqual(1*2*3*4*5*6*7*8*9*10*11, F.Fact(11));
+            // 8-ой тест: 15! 
+            Assert.AreEqual(1L*2*3*4*5*6*7*8*9*10*11*12*13*14*15, 
+                            F.Fact(15));
         }
+
+        /// <summary>
+        /// Числа Фибоначчи
+        /// </summary>
+        [Test]
+        public void TestFibonachi(){
+            Assert.AreEqual(1, F.Fib(1));
+            Assert.AreEqual(1, F.Fib(2));
+            Assert.AreEqual(1+1, F.Fib(3));
+            Assert.AreEqual(1+1+1, F.Fib(4));
+            Assert.AreEqual(433494437, F.Fib(43));
+            Assert.AreEqual(2880067194370816120, F.Fib(90));
+            
+            for(int i=1;;i++)
+                System.Console.WriteLine(""+i+"-ое число Фибоначчи = "+F.Fib(i));
+        }        
         
+        /// <summary>
+        /// Квадратное уравнение
+        /// </summary>
+        [Test]
+        public void TestSquareEq(){                               
+            F.checkEq(1,0,0);
+            F.checkEq(1,-2,1); // x^2 - 2*x + 1
+            F.checkEq(1,2,1); 
+            F.checkEq(1,0,4); // Нет решений
+            
+            F.checkEq(0,0,4); // Нет решений
+            Assert.AreEqual(0, F.roots(0,0,4).Length );
+            
+            F.checkEq(0,1,4); // Один корень 
+            Assert.AreEqual(1, F.roots(0,1,4).Length );
+
+            try{            
+                F.checkEq(0,0,0); // Бесконечное число корней
+                Assert.Fail("Ожидалось исключение");
+            } catch(Exception ex){
+                Assert.AreEqual("x-любое", ex.Message);
+            }
+        }
     }
 }
